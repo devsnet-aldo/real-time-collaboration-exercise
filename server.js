d72e1e7b37e327af0da60cf95445ea50454e51d2
Aldo Cortés <aacortes11@gmail.com>
2021-07-16 06:43:55 -0500

    Forcing webpack

--- server.js
+++ server.js
@@ -11,9 +11,5 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 app.use(express.static('./'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/'}
-  );
-});
 
 app.listen(process.env.PORT || 8080);
