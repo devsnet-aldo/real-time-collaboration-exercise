a0324cddf54a6fdee0e1635581d90e8b215e8cd1
Aldo Cortés <aacortes11@gmail.com>
2021-07-16 06:38:18 -0500

    Restore statics

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const express = require('express');
 
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static('./dist'));
+app.use(express.static('./'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/'}
+  );
+});
 
 app.listen(process.env.PORT || 8080);
